b3d849b5afd8569f8e26b08c79f73d3c16d32623
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-05-02 02:23:10 +0530

    responsive appllied

--- react-task/src/components/Footer.jsx
+++ react-task/src/components/Footer.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-import email from "../assets/email.png"
-import call from "../assets/call.png"
-import linkedin from "../assets/linkedin.png"
-import location from "../assets/location.png"
-import fb from "../assets/faceboook.png"
-import twitter from "../assets/twitter.png"
-import instagram from "../assets/instagram.png"
-import pinters from "../assets/pinteres.png"
-
+import email from "../assets/email.png";
+import call from "../assets/call.png";
+import linkedin from "../assets/linkedin.png";
+import location from "../assets/location.png";
+import fb from "../assets/faceboook.png";
+import twitter from "../assets/twitter.png";
+import instagram from "../assets/instagram.png";
+import pinters from "../assets/pinteres.png";
 
 function Footer() {
   return (
@@ -27,32 +26,33 @@ function Footer() {
       </div>
 
       <div id="footerMidSec">
+        <span>
+          {" "}
+          <b>FOLLOW US ON</b>
+        </span>
+        <div>
+          <img src={instagram} alt="" />
+          <p>Instagram</p>
+        </div>
 
-<span> <b>FOLLOW US ON</b></span>
-<div>
-    <img src={instagram} alt="" />
-    <p>Instagram</p>
-</div>
+        <div>
+          <img src={fb} alt="" />
 
-<div>
-<img src={fb} alt="" />
+          <p>Facebook</p>
+        </div>
 
-    <p>Facebook</p>
-</div>
+        <div>
+          <img src={twitter} alt="" />
 
-<div>
-<img src={twitter} alt="" />
+          <p>Twitter</p>
+        </div>
 
-    <p>Twitter</p>
-</div>
+        <div>
+          <img src={pinters} alt="" />
 
-<div>
-<img src={pinters} alt="" />
-
-    <p>Pinterest</p>
-</div>
-
-</div>
+          <p>Pinterest</p>
+        </div>
+      </div>
 
       <div id="footerRight">
         <span>
@@ -71,11 +71,15 @@ function Footer() {
           <p>Bangalore, Kr Puram</p>
         </div>
         <div>
-        <img src={linkedin} alt="" />
-          <a id="linkedin" href="https://www.linkedin.com/in/mohd-adil-634b0b241/">linkedin</a>
+          <img src={linkedin} alt="" />
+          <a
+            id="linkedin"
+            href="https://www.linkedin.com/in/mohd-adil-634b0b241/"
+          >
+            linkedin
+          </a>
         </div>
       </div>
-
     </div>
   );
 }
